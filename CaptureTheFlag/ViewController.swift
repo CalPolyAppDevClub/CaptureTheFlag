7880c4d112a9d929f809bf4984cf648fc09b1939
carlosaicrag <carlosaicrag@gmail.com>
2018-03-11 13:26:18 -0700

    UI Main View Controller
    Added google button option. Still trying t figure out how to show button.

--- CaptureTheFlag/ViewController.swift
+++ CaptureTheFlag/ViewController.swift
@@ -7,18 +7,23 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import GoogleSignIn
+class ViewController: CaptureTheFlagViewController, GIDSignInUIDelegate  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+        let googleButton = GIDSignInButton()
+        view.addSubview(googleButton)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
