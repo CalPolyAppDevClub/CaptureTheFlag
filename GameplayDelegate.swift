b9b1b7f7fa6bd9c6b560898b04930732c4e5d989
Ethan Abrams <ethanarigil56@gmail.com>
2018-08-20 11:33:25 -0700

    delete some files

--- GameplayDelegate.swift
+++
@@ -1,3 +0,0 @@
-protocol  GameplayDelegate {
-    func playerMovement(player: Player)
-}
--- Player.swift
+++
@@ -1,9 +0,0 @@
-class Player {
-    var name: String
-    var photo: URL
-    var location: GameLocation?
-
-    Player() {
-        //what is needed to init a player?
-    }
-}
